ef8a23ab46ef4b7da16bd2cbc829869449bd8726
josiahhuergo <joehuergo@gmail.com>
2024-05-31 23:00:36 -0400

    Fix transport control

--- index.ts
+++ index.ts
@@ -55,13 +55,12 @@ class NoteGrid {
     }
 
     #createFrame() {
-        console.log(this.pianoRoll);
         this.frame.id = "note-grid-frame"
         this.frame.style.position = "absolute";
-        this.frame.style.top = this.pianoRoll.timeBar.height + "px";
-        this.frame.style.left = this.pianoRoll.pianoBar.width + "px";
-        this.frame.style.width = this.pianoRoll.frameWidth + "px";
-        this.frame.style.height = this.pianoRoll.frameHeight + "px";
+        this.frame.style.top = this.pianoRoll.config.timeBarHeight + "px";
+        this.frame.style.left = this.pianoRoll.config.pianoBarWidth + "px";
+        this.frame.style.width = this.pianoRoll.config.frameWidth + "px";
+        this.frame.style.height = this.pianoRoll.config.frameHeight + "px";
         this.frame.style.overflow = "scroll"; 
         this.frame.style.scrollbarWidth = "none";
         this.pianoRoll.frame.append(this.frame);
@@ -88,7 +87,7 @@ class NoteGrid {
         let j = 0;
         for (let i=this.pianoRoll.maxDrawPitch; i>=this.pianoRoll.minDrawPitch; i--) { // Fill
             ctx.fillStyle = fillStyles[i%12];
-            ctx.fillRect(0, this.pianoRoll.noteHeight * j, canvas.width, this.pianoRoll.noteHeight);
+            ctx.fillRect(0, this.pianoRoll.config.noteHeight * j, canvas.width, this.pianoRoll.config.noteHeight);
             j++;
         }
         j=0;
@@ -99,8 +98,8 @@ class NoteGrid {
                 ctx.strokeStyle = colorTwo;
             }
             ctx.beginPath();
-            ctx.moveTo(0, j * this.pianoRoll.noteHeight);
-            ctx.lineTo(canvas.width, j * this.pianoRoll.noteHeight);
+            ctx.moveTo(0, j * this.pianoRoll.config.noteHeight);
+            ctx.lineTo(canvas.width, j * this.pianoRoll.config.noteHeight);
             ctx.stroke();
             j++;
         }
@@ -113,8 +112,8 @@ class NoteGrid {
                 ctx.strokeStyle = "rgb(40,40,40)";
             }
             ctx.beginPath();
-            ctx.moveTo(i*this.pianoRoll.beatWidth, 0);
-            ctx.lineTo(i*this.pianoRoll.beatWidth, canvas.height);
+            ctx.moveTo(i*this.pianoRoll.config.beatWidth, 0);
+            ctx.lineTo(i*this.pianoRoll.config.beatWidth, canvas.height);
             ctx.stroke();
         }
 
@@ -132,15 +131,15 @@ class NoteGrid {
         for (let i=0; i<this.pianoRoll.noteSet.length; i++) {
             let note: HTMLButtonElement = document.createElement("button");
             note.style.position = "absolute";
-            note.style.width = (this.pianoRoll.beatWidth * this.pianoRoll.noteSet[i].dur) + "px";
-            note.style.height = this.pianoRoll.noteHeight + "px";
-            note.style.left = (this.pianoRoll.beatWidth * this.pianoRoll.noteSet[i].time) + "px";
-            note.style.top = (this.pianoRoll.noteHeight * (this.pianoRoll.maxDrawPitch - this.pianoRoll.noteSet[i].pitch)) + "px";
+            note.style.width = (this.pianoRoll.config.beatWidth * this.pianoRoll.noteSet[i].dur) + "px";
+            note.style.height = this.pianoRoll.config.noteHeight + "px";
+            note.style.left = (this.pianoRoll.config.beatWidth * this.pianoRoll.noteSet[i].time) + "px";
+            note.style.top = (this.pianoRoll.config.noteHeight * (this.pianoRoll.maxDrawPitch - this.pianoRoll.noteSet[i].pitch)) + "px";
             note.innerHTML = this.pianoRoll.noteSet[i].pitch.toString();
             note.style.textAlign = "left";
             // note.style.verticalAlign = "middle";
             note.style.padding = 0+"px";
-            note.style.fontSize = clamp(this.pianoRoll.noteHeight/1.5, 0, 10) + "px";
+            note.style.fontSize = clamp(this.pianoRoll.config.noteHeight/1.5, 0, 10) + "px";
             this.noteGrid.append(note);
         }
     }
@@ -162,9 +161,9 @@ class PianoBar {
     #createFrame() {
         this.frame.id = "piano-bar-frame"
         this.frame.style.position = "absolute";
-        this.frame.style.top = this.pianoRoll.timeBar.height + "px";
+        this.frame.style.top = this.pianoRoll.config.timeBarHeight + "px";
         this.frame.style.width = this.width + "px";
-        this.frame.style.height = this.pianoRoll.frameHeight + "px";
+        this.frame.style.height = this.pianoRoll.config.frameHeight + "px";
         this.frame.style.overflow = "scroll";
         this.frame.style.scrollbarWidth = "none";
         this.pianoRoll.frame.append(this.frame);
@@ -191,7 +190,7 @@ class PianoBar {
         let j = 0;
         for (let i=this.pianoRoll.maxDrawPitch; i>=this.pianoRoll.minDrawPitch; i--) { // Fill
             ctx.fillStyle = fillStyles[i%12];
-            ctx.fillRect(0, this.pianoRoll.noteHeight * j, this.width, this.pianoRoll.noteHeight);
+            ctx.fillRect(0, this.pianoRoll.config.noteHeight * j, this.width, this.pianoRoll.config.noteHeight);
             j++;
         }
         j=0;
@@ -202,8 +201,8 @@ class PianoBar {
                 ctx.strokeStyle = colorTwo;
             }
             ctx.beginPath();
-            ctx.moveTo(0, j * this.pianoRoll.noteHeight);
-            ctx.lineTo(canvas.width, j * this.pianoRoll.noteHeight);
+            ctx.moveTo(0, j * this.pianoRoll.config.noteHeight);
+            ctx.lineTo(canvas.width, j * this.pianoRoll.config.noteHeight);
             ctx.stroke();
             j++;
         }
@@ -228,8 +227,8 @@ class TimeBar {
     #createFrame() {
         this.frame.id = "time-bar-frame";
         this.frame.style.position = "absolute";
-        this.frame.style.left = this.pianoRoll.pianoBar.width + "px";
-        this.frame.style.width = this.pianoRoll.frameWidth + "px";
+        this.frame.style.left = this.pianoRoll.config.pianoBarWidth + "px";
+        this.frame.style.width = this.pianoRoll.config.frameWidth + "px";
         this.frame.style.height = this.height + "px";
         this.frame.style.overflow = "scroll";
         this.frame.style.scrollbarWidth = "none";
@@ -248,8 +247,8 @@ class TimeBar {
         for (let i=0; i<this.pianoRoll.beatDrawCount; i++) {
             if (i%4 === 0) {
                 ctx.beginPath();
-                ctx.moveTo(i*this.pianoRoll.beatWidth, this.height / 3);
-                ctx.lineTo(i*this.pianoRoll.beatWidth, this.height);
+                ctx.moveTo(i*this.pianoRoll.config.beatWidth, this.height / 3);
+                ctx.lineTo(i*this.pianoRoll.config.beatWidth, this.height);
                 ctx.stroke();
 
                 let timeLabel = document.createElement("div");
@@ -257,9 +256,9 @@ class TimeBar {
                 timeLabel.style.fontFamily = "Verdana";
                 timeLabel.style.fontSize = "12px";
                 timeLabel.style.position = "absolute";
-                timeLabel.style.width = this.pianoRoll.beatWidth * 4 + "px";
+                timeLabel.style.width = this.pianoRoll.config.beatWidth * 4 + "px";
                 timeLabel.style.height = this.height + "px";
-                timeLabel.style.left = i*this.pianoRoll.beatWidth + "px";
+                timeLabel.style.left = i*this.pianoRoll.config.beatWidth + "px";
                 timeLabel.style.textAlign = "left";
                 timeLabel.style.paddingLeft = "5px";
                 timeLabel.style.paddingTop = "10px";
@@ -276,6 +275,7 @@ class Transport {
     pianoRoll: PianoRoll;
     ctx: CanvasRenderingContext2D;
     button: HTMLButtonElement;
+    bgColor: string = "rgb(20,20,20)";
 
     constructor(pianoRoll: PianoRoll) {
         this.pianoRoll = pianoRoll;
@@ -287,40 +287,27 @@ class Transport {
         this.button.style.position = "absolute";
         this.button.style.top = "0px";
         this.button.style.left = "0px";
-        this.button.style.width = this.pianoRoll.pianoBar.width + "px";
-        this.button.style.height = this.pianoRoll.timeBar.height + "px";
+        this.button.style.width = this.pianoRoll.config.pianoBarWidth + "px";
+        this.button.style.height = this.pianoRoll.config.timeBarHeight + "px";
         this.button.style.border = "none";
         this.button.style.backgroundColor = "black";
-        
-        this.button.addEventListener('click', () => { this.#toggle.bind(this) });
+        this.button.style.zIndex = "9";
 
         canvas.style.position = "absolute";
         canvas.style.top = "0px";
         canvas.style.left = "0px";
-        canvas.width = this.pianoRoll.pianoBar.width;
-        canvas.height = this.pianoRoll.timeBar.height;
+        canvas.width = this.pianoRoll.config.pianoBarWidth;
+        canvas.height = this.pianoRoll.config.timeBarHeight;
 
-        this.#drawPlay();        
+        this.drawPlay();        
 
         this.button.append(canvas);
         this.pianoRoll.frame.append(this.button);
     }
 
-    #toggle() {
-        if (this.pianoRoll.playing) { 
-            this.pianoRoll.playing = false;
-            this.#drawPlay();
-            this.pianoRoll.playHead.toggle();
-        } else {
-            this.pianoRoll.playing = true;
-            this.#drawStop();
-            this.pianoRoll.playHead.toggle();
-        }
-    }
-
-    #drawPlay() {
-        this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0, 0, this.pianoRoll.pianoBar.width, this.pianoRoll.timeBar.height);
+    drawPlay() {
+        this.ctx.fillStyle = this.bgColor;
+        this.ctx.fillRect(0, 0, this.pianoRoll.config.pianoBarWidth, this.pianoRoll.config.timeBarHeight);
         this.ctx.beginPath();
         this.ctx.moveTo(8,7);
         this.ctx.lineTo(8,23);
@@ -332,9 +319,9 @@ class Transport {
         this.ctx.stroke();
     }
 
-    #drawStop() {
-        this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0, 0, this.pianoRoll.pianoBar.width, this.pianoRoll.timeBar.height);
+    drawStop() {
+        this.ctx.fillStyle = this.bgColor;
+        this.ctx.fillRect(0, 0, this.pianoRoll.config.pianoBarWidth, this.pianoRoll.config.timeBarHeight);
         this.ctx.beginPath();
         this.ctx.moveTo(8,7);
         this.ctx.lineTo(8,23);
@@ -348,31 +335,30 @@ class Transport {
     }
 }
 
+class Configuration {
+    frameWidth: number = 800;
+    frameHeight: number = 600;
+    bpm: number = 120;
+    beatWidth: number = 28;
+    noteHeight: number = 18;
+    pianoBarWidth: number = 30;
+    timeBarHeight: number = 30;
+}
+
 class PianoRoll {
     frame: HTMLDivElement;
-    frameWidth: number;
-    frameHeight: number;
-    bpm: number;
-    playing: boolean;
     noteSet: Note[];
-    beatWidth: number;
-    noteHeight: number;
+    config: Configuration;
     noteGrid: NoteGrid;
     pianoBar: PianoBar;
     timeBar: TimeBar;
-    // transport: Transport;
+    transport: Transport;
     playHead: PlayHead;
-    ignore: boolean;
+    playing: boolean = false;
+    scrollSync: boolean = false;
 
-    constructor(noteSet: Note[] = [], frameWidth: number = 800, frameHeight: number = 600, beatWidth: number = 28, noteHeight: number = 18) {
+    constructor(noteSet: Note[] = []) {
         this.noteSet = noteSet;
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
-        this.beatWidth = beatWidth;
-        this.noteHeight = noteHeight;
-        this.bpm = 120;
-        this.ignore = false;
-        this.playing = false;
 
         // Remove notes that can't be displayed
         let newNoteSet: Note[] = [];
@@ -385,12 +371,15 @@ class PianoRoll {
         }
         this.noteSet = newNoteSet;
 
+        // Create config
+        this.config = new Configuration();
+
         // Create entire frame
         this.frame = document.createElement("div");
         this.#createFrame();
 
         // Create transport controls
-        // this.transport = new Transport(this);  
+        this.transport = new Transport(this);  
         // Create Time Bar
         this.timeBar = new TimeBar(this);
         // Create piano bar
@@ -403,6 +392,13 @@ class PianoRoll {
         // Scroll sync
         this.#syncScrolling();
         this.#scrollToFirstNote();
+
+        this.#setupListeners();
+    }
+
+    #setupListeners() {
+        this.transport.button.addEventListener("click", this.toggle.bind(this));
+        document.body.addEventListener("keydown", this.toggle.bind(this));
     }
 
     #createFrame() {
@@ -418,60 +414,72 @@ class PianoRoll {
         // Ideally, when one frame is scrolled, we'd remove the event listener from the other frame, 
         // perform the scroll, set scrollTop, then add the other event listener again.
         //
-        this.ignore = false; // the ignore flag is something whose state needs to be stored in the object, 
+        this.scrollSync = false; // the scrollSync flag is something whose state needs to be stored in the object, 
         // allowing the firing of a frame's scroll event to disable the other frame's scroll event.
         this.noteGrid.frame.addEventListener("scroll", (event) => {
-            if (this.ignore) {
-                this.ignore = false;
+            if (this.scrollSync) {
+                this.scrollSync = false;
                 return;
             }
-            this.ignore = true;
+            this.scrollSync = true;
             this.pianoBar.frame.scrollTop = this.noteGrid.frame.scrollTop;
             this.timeBar.frame.scrollLeft = this.noteGrid.frame.scrollLeft;
         });
         this.pianoBar.frame.addEventListener("scroll", (event) => {
-            if (this.ignore) {
-                this.ignore = false;
+            if (this.scrollSync) {
+                this.scrollSync = false;
                 return;
             }
-            this.ignore = true;
+            this.scrollSync = true;
             this.noteGrid.frame.scrollTop = this.pianoBar.frame.scrollTop;
         });
         this.timeBar.frame.addEventListener("scroll", (event) => {
-            if (this.ignore) {
-                this.ignore = false;
+            if (this.scrollSync) {
+                this.scrollSync = false;
                 return;
             }
-            this.ignore = true;
+            this.scrollSync = true;
             this.noteGrid.frame.scrollLeft = this.timeBar.frame.scrollTop;
         });
     }
 
     #scrollToFirstNote() {
-        let noteYPos = (this.maxDrawPitch - this.pitches[0]) * this.noteHeight;
+        let noteYPos = (this.maxDrawPitch - this.noteSet[0].pitch) * this.config.noteHeight;
         let halfPageY = parseInt(this.noteGrid.frame.style.height) / 2;
         noteYPos = noteYPos - halfPageY;
         this.noteGrid.frame.scrollTop = noteYPos;
     }
 
+    play() {
+        this.playing = true;
+        this.transport.drawStop();
+        this.playHead.play();
+    }
+
+    stop() {
+        this.playing = false;
+        this.transport.drawPlay();
+        this.playHead.stop();
+    }
+
+    toggle() {
+        if (!this.playing) {
+            this.play();
+        } else {
+            this.stop();
+        }
+    }
+
     get canvasHeight() {
-        return this.noteHeight * (this.maxDrawPitch - this.minDrawPitch + 1);
+        return this.config.noteHeight * (this.maxDrawPitch - this.minDrawPitch + 1);
     }
 
     get canvasWidth() {
         if (this.beatsFitInFrame) {
-            return this.frameWidth;
+            return this.config.frameWidth;
         } else {
-            return this.beatWidth * this.beatDrawCount;
+            return this.config.beatWidth * this.beatDrawCount;
         }
-    }
-
-    get pitches() {
-        let pitches: number[] = [];
-        this.noteSet.forEach(function(note: Note) {
-            pitches.push(note.pitch);
-        });
-        return pitches;
     }
 
     get times() {
@@ -487,22 +495,11 @@ class PianoRoll {
     }
 
     get maxDrawPitch() {
-        //return max(this.pitches) + this.pitchMargins;
         return 108;
     }
 
-    get pitchesFitInFrame() {
-        let heightInPitches = Math.ceil(this.frameHeight / this.noteHeight);
-        // if difference between max(this.pitches) and min(this.pitches) is less than heightInPitches, return Math.ceil etc...
-        if (this.pitchRange < heightInPitches) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     get beatsFitInFrame() {
-        let widthInBeats = Math.ceil(this.frameWidth / this.beatWidth);
+        let widthInBeats = Math.ceil(this.config.frameWidth / this.config.beatWidth);
         if ((max(this.times) - min(this.times)) < widthInBeats) {
             return true;
         } else {
@@ -510,13 +507,9 @@ class PianoRoll {
         }
     }
 
-    get pitchRange() {
-        return max(this.pitches) - min(this.pitches);
-    }
-
     get beatDrawCount() {
         if (this.beatsFitInFrame) {
-            return Math.ceil(this.frameWidth / this.beatWidth);
+            return Math.ceil(this.config.frameWidth / this.config.beatWidth);
         } else {
             return this.noteSet.slice(-1)[0].time + this.noteSet.slice(-1)[0].dur;
         }
@@ -525,27 +518,23 @@ class PianoRoll {
 
 class PlayHead {
     pianoRoll: PianoRoll;
-    playing: boolean;
-    startTime: number;
-    headPos: number;
-    playTime: number;
+    startTime: number = 0;
+    headPos: number = 0;
+    playTime: number = 0;
     noteCtx: CanvasRenderingContext2D;
-    // timeCtx: CanvasRenderingContext2D;
+    // timeCtx: CanvasRenderingContext2D; // render on time bar too
 
     constructor(pianoRoll: PianoRoll) {
         this.pianoRoll = pianoRoll;
-        this.playing = false;
-        this.startTime = 0; // Start position
-        this.headPos = 0; // Playhead drawn position
 
         var noteCanvas: HTMLCanvasElement = document.createElement("canvas");
-        noteCanvas.style.position = "absolute";
         noteCanvas.id = "playhead"
+        noteCanvas.style.position = "absolute";
         this.noteCtx = noteCanvas.getContext("2d")!;
         noteCanvas.width = this.pianoRoll.canvasWidth;
         noteCanvas.height = this.pianoRoll.canvasHeight;
-        this.playTime = 0;
         this.update = this.update.bind(this);
+
         this.pianoRoll.noteGrid.frame.append(noteCanvas);
 
         this.drawHead();
@@ -565,37 +554,27 @@ class PlayHead {
     }
 
     play() {
-        this.playing = true;
         requestAnimationFrame(this.update);
     }
 
     stop() {
-        this.playing = false;
         this.headPos = this.startTime;
         this.playTime = 0;
         this.drawHead();
     }
 
-    toggle() {
-        if (this.playing) {
-            this.stop();
-        } else {
-            this.play();
-        }
-    }
-
     update(now: any) {
-        if (!this.playing) return;
+        if (!this.pianoRoll.playing) return;
 
         if (!this.playTime) { this.playTime = now }
         let elapsed = now - this.playTime;
-        this.headPos = (elapsed/200) * this.pianoRoll.beatWidth;
+        this.headPos = (elapsed/200) * this.pianoRoll.config.beatWidth;
 
         if (this.headPos >= this.pianoRoll.canvasWidth) {
             this.stop();
         }
         
-        if (this.playing) {
+        if (this.pianoRoll.playing) {
             this.drawHead();
             requestAnimationFrame(this.update);
         }
@@ -605,15 +584,9 @@ class PlayHead {
 
 var pianoRoll: PianoRoll;
 
-$(document).ready(function() {    
+$(function() {    
     let notes = makeNotes();
     pianoRoll = new PianoRoll(notes);
-
-    document.body.onkeydown = function(e) {
-        if (e.key == " ") {
-            pianoRoll.playHead.toggle();
-        }
-    }
 });
 
 
